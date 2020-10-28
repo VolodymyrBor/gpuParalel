be2590956cb17c861d08d7c21dfa34fef318c27c
Volodymyr Borysiuk <volodymyr.borysiuk0@gmail.com>
2020-10-28 21:53:34 +0200

    added compiler to PATH; refactor pycuda kernel.

--- matrix/common.py
+++ matrix/common.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from contextlib import contextmanager
 
@@ -19,3 +20,10 @@ def measure_time(text: str):
         finish = datetime.now()
         delta = finish - start
         print(f'{text} finish at: {finish}. Delta: {delta.microseconds}.')
+
+
+def add_compiler_to_PATH():
+    if os.system("cl.exe"):
+        os.environ['PATH'] += ';' + r"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Tools\MSVC\14.27.29110\bin\Hostx64\x64"
+    if os.system("cl.exe"):
+        raise RuntimeError("cl.exe still not found, path probably incorrect")
